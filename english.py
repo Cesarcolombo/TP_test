f15d0625889f57d59acfc90a14ff71df91b31c26
Cesar Colombo <cesarcolombo3@gmail.com>
2025-12-09 16:03:48 +0100

    english fonctionne mais echoue le test

--- english.py
+++ english.py
@@ -1,6 +1,9 @@
 # pylint: disable=missing-docstring
 
 from utils import Cli
+# pylint: disable=missing-docstring
+
+from itertools import count
 
 
 class EnglishAuction():
@@ -9,7 +12,49 @@ class EnglishAuction():
         self.cli = cli if cli else Cli()
 
     def play(self):
-        pass
+        # Input opening bid
+        self.cli.display('Started auction of type: Blind')
+        opening_bid = self.cli.prompt('Please enter the amount for the opening bid:')
+        opening_bid = int(opening_bid)
+        self.cli.display(f"Opening bid is: {opening_bid}")
+
+        # Input bidders
+        bidders = []
+        for index in count(1):
+            bidder = self.cli.prompt(
+                f"Enter name for bidder {index} (enter nothing to move on):"
+            )
+            if not bidder:
+                break
+            bidders.append(bidder)
+        self.cli.display(f"\nBidders are: {', '.join(bidders)}\n")
+
+        # Collect bids
+        standing_bid = opening_bid
+        winner = None
+        leader = None
+        played=True
+        while played:
+            played = False 
+            for bidder in bidders:
+                bid = self.cli.prompt(
+                    f"Standing bid is {standing_bid}. {bidder} bids:"
+                )
+                if bid == '':
+                    continue
+                bid = int(bid)
+                if bid > standing_bid:
+                    standing_bid = bid
+                    leader = bidder
+                    played = True
+        winner = leader
+
+
+
+        # Display winner
+        self.cli.display("\n~~~~~~~~\n")
+        self.cli.display(f"Winner is {winner}. Winning bid is {standing_bid}.")
+
 
 
 if __name__ == "__main__":
